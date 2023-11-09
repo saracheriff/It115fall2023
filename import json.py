82cd87de303f87eaf1e6abefec4a9c122709eee1
Sara Cherif <saracherif@Saras-MacBook-Pro.local>
2023-11-09 09:57:42 -0800

    first commit

---
+++ import json.py
@@ -0,0 +1,37 @@
+import json
+
+# Sample Data
+data = {
+      'name' : 'Massandje Cherif',
+      'age': 22,
+      'city': 'Seattle,WA',
+      'interests': ['Makeup','Skincare','Reading'],
+      'is_student' : True
+
+}
+# Writing data to a JSON file
+with open('data.json', 'w') as json_file:
+     
+     json.dump(data, json_file, indent=4)
+
+print('Data has been written to data.json')
+
+
+#Reading from the JSON file
+with open('data.json','r') as json_file:
+     #Load the data
+      loaded_data =  json.load(json_file)
+
+print('Data loaded from data.json')
+print(loaded_data)
+
+
+  ##
+loaded_data['age'] = 28
+loaded_data['interests'].append('I like pink blush')
+
+
+#
+with open('data.json', 'w') as json_file:
+    json.dump(loaded_data, json_file, indent=4)
+print('Modified data to the data.json file')
